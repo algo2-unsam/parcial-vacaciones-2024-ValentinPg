14174773e63fa416c3a26b2bd4502e4a69721730
ValenPg <valentinpugliesew@outlook.com>
2024-06-06 19:16:51 -0300

    Avance con el punto 3

--- src/main/kotlin/parcial/ArmadorTour.kt
+++ src/main/kotlin/parcial/ArmadorTour.kt
@@ -1,8 +1,40 @@
 package parcial
 
+import java.time.LocalDate
+
 class ArmadorTour {
+    val listaPersonas = mutableListOf<Persona>()
+    val listaPersonasPendientes = mutableListOf<Persona>()
+    val listaTours = mutableListOf<Tour>()
+
+
+    fun agregarTour(tour: Tour) {
+        listaTours.add(tour)
+    }
+
+    fun armarTours() {
+        listaPersonas.forEach {}
+    }
+
+    fun ordenarXPrecioAsc(): List<Tour> = listaTours.sortedBy { it.montoXPersona }
+
+    fun toursDisponibles(persona: Persona): List<Tour> = listaTours.filter { it.esAdecuadoPara(persona) }
 }
 
-class Tour{
+class Tour(
+    val fechaSalida: LocalDate,
+    var cantidadPersonasRequeridas: Int,
+    val lugaresARecorrer: List<Lugar>,
+    var montoXPersona: Double
+) {
+    val personasABordo: MutableList<Persona> = mutableListOf()
 
+    fun confirmar() {}
+
+    fun esAdecuadoPara(persona: Persona): Boolean =
+        lugaresARecorrer.all { persona.esLugarAdecuado(it) && montoXPersona <= persona.presupuesto }
+
+    fun agregarPersona(persona: Persona) {
+        personasABordo.add(persona)
+    }
 }
--- src/main/kotlin/parcial/Persona.kt
+++ src/main/kotlin/parcial/Persona.kt
@@ -1,11 +1,13 @@
 package parcial
 
 //Punto 2
-class Persona {
+class Persona(var presupuesto: Double) {
     //Implemento un strategy, lo que me va a permitir cambiar facilmente la preferencia del Usuario
     lateinit var preferenciaVacaciones: PreferenciaVacaciones
 
     fun cambiarPreferenciaVacaciones(nuevaPreferencia: PreferenciaVacaciones) {
         preferenciaVacaciones = nuevaPreferencia
     }
+
+    fun esLugarAdecuado(lugar: Lugar): Boolean = preferenciaVacaciones.esAdecuado(lugar)
 }
