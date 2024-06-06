292e20aa998232ecd03c7e6778d81f473ae02444
ValenPg <valentinpugliesew@outlook.com>
2024-06-06 19:36:38 -0300

    Punto 3 listo

--- src/main/kotlin/parcial/ArmadorTour.kt
+++ src/main/kotlin/parcial/ArmadorTour.kt
@@ -13,10 +13,18 @@ class ArmadorTour {
     }
 
     fun armarTours() {
-        listaPersonas.forEach {}
+        listaPersonas.forEach { asignarTour(it, toursDisponibles(it)) }
     }
 
-    fun ordenarXPrecioAsc(): List<Tour> = listaTours.sortedBy { it.montoXPersona }
+    private fun asignarTour(persona: Persona, toursDisponibles: List<Tour>) {
+        if (toursDisponibles.isEmpty()) {
+            listaPersonasPendientes.add(persona)
+        } else {
+            ordenarXPrecioAsc(toursDisponibles).first().agregarPersona(persona)
+        }
+    }
+
+    fun ordenarXPrecioAsc(tours: List<Tour>): List<Tour> = tours.sortedBy { it.montoXPersona }
 
     fun toursDisponibles(persona: Persona): List<Tour> = listaTours.filter { it.esAdecuadoPara(persona) }
 }
@@ -29,12 +37,17 @@ class Tour(
 ) {
     val personasABordo: MutableList<Persona> = mutableListOf()
 
-    fun confirmar() {}
 
     fun esAdecuadoPara(persona: Persona): Boolean =
-        lugaresARecorrer.all { persona.esLugarAdecuado(it) && montoXPersona <= persona.presupuesto }
+        lugaresARecorrer.all { persona.esLugarAdecuado(it) } && montoXPersona <= persona.presupuesto && hayEspacioLibre()
+
+    private fun hayEspacioLibre(): Boolean = personasABordo.size < cantidadPersonasRequeridas
 
     fun agregarPersona(persona: Persona) {
         personasABordo.add(persona)
+    }
+
+    fun eliminarPersona(persona: Persona) {
+        personasABordo.remove(persona)
     }
 }
